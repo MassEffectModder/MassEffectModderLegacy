25bf62873bd3fbcfc43b9d2e3843b1b27a3be325
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2016-05-29 17:50:35 +0200

    update copyright year

--- MassEffectModder/IniConf.cs
+++ MassEffectModder/IniConf.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * MassEffectModder
  *
- * Copyright (C) 2014-2015 Pawel Kolodziejski <aquadran at users.sourceforge.net>
+ * Copyright (C) 2014-2016 Pawel Kolodziejski <aquadran at users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
