82359cb49496be29abb8afe58ff6734a146f69da
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2016-07-13 06:40:41 +0200

    fixed missing size of texture storing

--- MassEffectModder/TexExplorer.cs
+++ MassEffectModder/TexExplorer.cs
@@ -726,6 +726,9 @@ namespace MassEffectModder
                         break;
                 }
                 texture.replaceMipMaps(mipmaps);
+                texture.properties.setIntValue("SizeX", texture.mipMapsList.First().width);
+                texture.properties.setIntValue("SizeY", texture.mipMapsList.First().height);
+                texture.properties.setIntValue("MipTailBaseIdx", texture.mipMapsList.Count() - 1);
 
                 MemoryStream newData = new MemoryStream();
                 newData.WriteFromBuffer(texture.properties.toArray());
