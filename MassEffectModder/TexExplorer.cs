15c89516d3e215cdd1d10db1b71d37e47e7bc57c
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2016-07-24 13:47:02 +0200

    fixed path for DLC tfc

--- MassEffectModder/TexExplorer.cs
+++ MassEffectModder/TexExplorer.cs
@@ -1091,7 +1091,7 @@ namespace MassEffectModder
                     if (nodeTexture.path.Contains("DLC"))
                     {
                         string DLCname = Path.GetDirectoryName(Path.GetDirectoryName(nodeTexture.path)).Split('\\').Last();
-                        archiveFile = Path.Combine(Path.GetDirectoryName((GameData.GamePath + nodeTexture.path)), archive + "_" + DLCname + ".tfc");
+                        archiveFile = Path.Combine(Path.GetDirectoryName((GameData.GamePath + nodeTexture.path)), archive + ".tfc");
                         if (!File.Exists(archiveFile))
                             archiveFile = Path.Combine(GameData.MainData, archive + ".tfc");
                     }
