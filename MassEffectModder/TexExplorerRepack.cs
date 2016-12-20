31c7df3b336534480e9ec441020a470a10bbc3a3
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2016-12-20 19:36:21 +0100

    Added WIP code to repo

--- MassEffectModder/TexExplorerRepack.cs
+++ MassEffectModder/TexExplorerRepack.cs
@@ -23,6 +23,7 @@ using StreamHelpers;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace MassEffectModder
@@ -31,21 +32,119 @@ namespace MassEffectModder
     {
         private void RepackTexturesTFC()
         {
-            DialogResult result = MessageBox.Show("Repacking textures can be very long process.\n\n" +
+            DialogResult result = MessageBox.Show("Repacking textures can be long time process.\n\n" +
                 "Are you sure to proceed?", "Textures repacking (WIP feature!)", MessageBoxButtons.YesNo);
             if (result == DialogResult.No)
             {
                 Close();
                 return;
             }
-
-            for (int i = 0; i < nodeList.Count; i++)
+/*
+            if (_gameSelected == MeType.ME2_TYPE)
             {
-                PackageTreeNode node = nodeList[i];
+                for (int i = 0; i < _textures.Count; i++)
                 {
+                    FoundTexture foundTexture = _textures[i];
+                    _mainWindow.updateStatusLabel("Texture: " + (i + 1) + " of " + _textures.Count);
+                    _mainWindow.updateStatusLabel2("");
+                    string TFCfilename = "", TFCfilenameTemp, prevTFCfilename = "";
+                    Texture prevTexture = null;
+                    for (int index2 = 0; index2 < foundTexture.list.Count; index2++)
+                    {
+                        MatchedTexture matchedTexture = foundTexture.list[index2];
+                        string packagePath = GameData.GamePath + matchedTexture.path;
+                        Package package = cachePackageMgr.OpenPackage(packagePath);
+                        Texture texture = new Texture(package, matchedTexture.exportID, package.getExportData(matchedTexture.exportID));
+                        if (!texture.mipMapsList.Exists(b => b.storageType == Texture.StorageTypes.extLZO || b.storageType == Texture.StorageTypes.extUnc || b.storageType == Texture.StorageTypes.extZlib))
+                            continue;
+
+                        bool modified = false;
+                        for (int l = 0; l < texture.mipMapsList.Count; l++)
+                        {
+                            Texture.MipMap mipmap = texture.mipMapsList[l];
+                            if (((int)mipmap.storageType & (int)Texture.StorageFlags.externalFile) != 0)
+                            {
+                                string archive = texture.properties.getProperty("TextureFileCacheName").valueName;
+                                TFCfilename = Path.Combine(GameData.MainData, archive + ".tfc");
+                                if (packagePath.Contains("\\DLC"))
+                                {
+                                    string DLCArchiveFile = Path.Combine(Path.GetDirectoryName(packagePath), archive + ".tfc");
+                                    if (File.Exists(DLCArchiveFile))
+                                        TFCfilename = DLCArchiveFile;
+                                    else if (GameData.gameType == MeType.ME2_TYPE)
+                                        TFCfilename = Path.Combine(GameData.MainData, "Textures.tfc");
+                                }
+
+                                TFCfilenameTemp = TFCfilename + ".TempTFC";
+                                if (!File.Exists(TFCfilenameTemp))
+                                {
+                                    using (FileStream fs = new FileStream(TFCfilenameTemp, FileMode.CreateNew, FileAccess.Write))
+                                    {
+                                        byte[] guid = new byte[16];
+                                        Array.Copy(texture.properties.getProperty("TFCFileGuid").valueStruct, guid, 16);
+                                        fs.WriteFromBuffer(guid);
+                                    }
+                                }
+
+                                if (TFCfilename != prevTFCfilename)
+                                {
+                                    using (FileStream fs = new FileStream(TFCfilename, FileMode.Open, FileAccess.Read))
+                                    {
+                                        fs.JumpTo(mipmap.dataOffset);
+                                        using (FileStream fsNew = new FileStream(TFCfilenameTemp, FileMode.Append, FileAccess.Write))
+                                        {
+                                            fsNew.SeekEnd();
+                                            mipmap.dataOffset = (uint)fsNew.Position;
+                                            if (mipmap.storageType == Texture.StorageTypes.extLZO ||
+                                                mipmap.storageType == Texture.StorageTypes.extZlib)
+                                            {
+                                                fsNew.WriteFromStream(fs, mipmap.compressedSize);
+                                            }
+                                            else
+                                            {
+                                                fsNew.WriteFromStream(fs, mipmap.uncompressedSize);
+                                            }
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    Texture.MipMap tmpMipmap = prevTexture.getMipmap(mipmap.width, mipmap.height);
+                                    if (mipmap.storageType != tmpMipmap.storageType)
+                                        throw new Exception("");
+                                    mipmap.dataOffset = tmpMipmap.dataOffset;
+                                }
+                                texture.mipMapsList[l] = mipmap;
+                                modified = true;
+                            }
+                        }
+
+                        if (modified)
+                        {
+                            prevTFCfilename = TFCfilename;
+                            prevTexture = texture;
+                            using (MemoryStream newData = new MemoryStream())
+                            {
+                                newData.WriteFromBuffer(texture.properties.toArray());
+                                newData.WriteFromBuffer(texture.toArray(package.exportsTable[matchedTexture.exportID].dataOffset + (uint)newData.Position));
+                                //package.setExportData(nodeTexture.exportID, newData.ToArray());
+                            }
+                        }
+                    }
                 }
             }
+            _mainWindow.updateStatusLabel("");
 
+            List<string> tfcFles = Directory.GetFiles(GameData.GamePath, "*.TempTFC", SearchOption.AllDirectories).ToList();
+            for (int i = 0; i < tfcFles.Count; i++)
+            {
+
+                string newname = tfcFles[i].Substring(0, tfcFles[i].IndexOf(".TempTFC"));
+                File.Delete(newname);
+                File.Move(tfcFles[i], newname);
+            }
+            //cachePackageMgr.CloseAllWithSave();
+            */
             _mainWindow.updateStatusLabel("Done.");
             _mainWindow.updateStatusLabel("");
         }
