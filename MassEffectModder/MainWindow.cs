71202119bfcb7e8699d53008f8153ba8439afe0e
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2017-02-02 04:41:44 +0100

    Added MD5 sums for older version of DLC

--- MassEffectModder/MainWindow.cs
+++ MassEffectModder/MainWindow.cs
@@ -41,7 +41,7 @@ namespace MassEffectModder
         public MainWindow(bool runAsAdmin)
         {
             InitializeComponent();
-            Text = "Mass Effect Modder v1.68";
+            Text = "Mass Effect Modder v1.69";
             if (runAsAdmin)
                 Text += " (run as Administrator)";
             _configIni = new ConfIni();
