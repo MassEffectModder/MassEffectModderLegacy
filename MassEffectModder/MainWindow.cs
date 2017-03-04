2e27e41ed1f8c447d918e1a26c7eebefa02073e0
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2017-03-04 18:07:53 +0100

    bump version

--- MassEffectModder/MainWindow.cs
+++ MassEffectModder/MainWindow.cs
@@ -41,7 +41,7 @@ namespace MassEffectModder
         public MainWindow(bool runAsAdmin)
         {
             InitializeComponent();
-            Text = "Mass Effect Modder v1.93";
+            Text = "Mass Effect Modder v1.94";
             if (runAsAdmin)
                 Text += " (run as Administrator)";
             _configIni = new ConfIni();
