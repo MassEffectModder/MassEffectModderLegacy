c8ab923be7732bd1f17797dd9633076199bac8e4
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2017-07-28 08:17:14 +0200

    implemented self update

--- MassEffectModder/Program.cs
+++ MassEffectModder/Program.cs
@@ -24,8 +24,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace MassEffectModder
@@ -53,6 +56,8 @@ namespace MassEffectModder
         public static byte[] tableME1 = null;
         public static byte[] tableME2 = null;
         public static byte[] tableME3 = null;
+        private static Form progressForm;
+        private static ProgressBar progressBar;
 
         static void loadEmbeddedDlls()
         {
@@ -178,6 +183,111 @@ namespace MassEffectModder
             }
         }
 
+        static private string prepareForUpdate()
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Headers["Accept"] = "application/vnd.github.v3+json";
+                webClient.Headers["user-agent"] = "MEM";
+                string releaseString = webClient.DownloadString("https://api.github.com/repos/MassEffectModder/MassEffectModder/releases/latest");
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                var parsed = serializer.Deserialize<Dictionary<string, dynamic>>(releaseString);
+                string githubVersion = parsed["tag_name"];
+                if (int.Parse(githubVersion) > int.Parse(Application.ProductVersion))
+                {
+                    if (MessageBox.Show("New version of MEM is available: " + githubVersion + "\nDo you want update to new version?", "Update", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        progressBar = new ProgressBar();
+                        progressBar.Size = new System.Drawing.Size(400, 40);
+                        progressBar.Style = ProgressBarStyle.Marquee;
+                        progressBar.MarqueeAnimationSpeed = 1;
+                        progressForm = new Form();
+                        progressForm.StartPosition = FormStartPosition.CenterScreen;
+                        progressForm.Size = new System.Drawing.Size(400, 60);
+                        progressForm.MinimumSize = new System.Drawing.Size(400, 60);
+                        progressForm.MaximumSize = new System.Drawing.Size(400, 60);
+                        progressForm.MinimizeBox = false;
+                        progressForm.MaximizeBox = false;
+                        progressForm.ControlBox = false;
+                        progressForm.Controls.Add(progressBar);
+                        progressForm.Show();
+
+                        Uri url = new Uri(parsed["assets"][0]["browser_download_url"]);
+                        string filename = parsed["assets"][0]["name"];
+                        webClient.DownloadFileAsync(url, filename);
+                        while (webClient.IsBusy) { Application.DoEvents(); Thread.Sleep(10); }
+
+                        progressForm.Close();
+                        return filename;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Failed checking latest available program version!");
+            }
+            return "";
+        }
+
+        static private string unpackUpdate(string file)
+        {
+            IntPtr handle = IntPtr.Zero;
+            int result;
+            ulong numEntries = 0;
+            string fileName = "";
+            string feleNameExe = "";
+            uint dstLen = 0;
+            try
+            {
+                byte[] buffer = File.ReadAllBytes(file);
+                handle = ZlibHelper.Zip.Open(buffer, ref numEntries, 0);
+                for (uint i = 0; i < numEntries; i++)
+                {
+                    result = ZlibHelper.Zip.GetCurrentFileInfo(handle, ref fileName, ref dstLen);
+                    if (result != 0)
+                        throw new Exception();
+
+                    byte[] data = new byte[dstLen];
+                    result = ZlibHelper.Zip.ReadCurrentFile(handle, data, dstLen);
+                    if (result != 0)
+                    {
+                        throw new Exception();
+                    }
+                    if (File.Exists("new-" + fileName))
+                        File.Delete("new-" + fileName);
+                    using (FileStream fs = new FileStream("new-" + fileName, FileMode.CreateNew))
+                    {
+                        fs.WriteFromBuffer(data);
+                    }
+                    if (Path.GetExtension(fileName).ToLowerInvariant() == ".exe")
+                        feleNameExe = "new-" + fileName;
+
+                    ZlibHelper.Zip.GoToNextFile(handle);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Failed unpack update!");
+            }
+
+            if (handle != IntPtr.Zero)
+                ZlibHelper.Zip.Close(handle);
+
+            return feleNameExe;
+        }
+
+        static private void cleanupPreviousUpdate()
+        {
+            string baseName = "new-" + Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+            string fileExe = baseName + ".exe";
+            string filePdb = baseName + ".pdb";
+            if (File.Exists(fileExe))
+                File.Delete(fileExe);
+            if (File.Exists(filePdb))
+                File.Delete(filePdb);
+        }
+
         [STAThread]
 
         static void Main(string[] args)
@@ -194,6 +304,40 @@ namespace MassEffectModder
             {
                 cmd = args[0];
                 loadEmbeddedDlls();
+            }
+
+            if (cmd.Equals("-update-mem", StringComparison.OrdinalIgnoreCase))
+            {
+                ShowWindow(GetConsoleWindow(), 0);
+                try
+                {
+                    if (args.Length != 2)
+                        throw new Exception();
+                    string baseName = Path.GetFileNameWithoutExtension(args[1]);
+                    string fileExe = baseName.Substring("new-".Length) + ".exe";
+                    string filePdb = baseName.Substring("new-".Length) + ".pdb";
+                    if (File.Exists(fileExe))
+                        File.Delete(fileExe);
+                    if (File.Exists(filePdb))
+                        File.Delete(filePdb);
+                    File.Copy(baseName + ".exe", fileExe);
+                    File.Copy(baseName + ".pdb", filePdb);
+
+                    Process process = new Process();
+                    process.StartInfo.FileName = fileExe;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.UseShellExecute = false;
+                    if (!process.Start())
+                        throw new Exception();
+
+                    unloadEmbeddedDlls();
+                    Environment.Exit(0);
+                }
+                catch
+                {
+                    MessageBox.Show("Failed update MEM!");
+                    goto fail;
+                }
             }
 
             if (cmd.Equals("-convert-to-mem", StringComparison.OrdinalIgnoreCase) ||
@@ -393,6 +537,25 @@ namespace MassEffectModder
 
                 loadEmbeddedDlls();
 
+
+                cleanupPreviousUpdate();
+                string filename = prepareForUpdate();
+                filename = unpackUpdate(filename);
+                if (filename != "")
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = filename;
+                    process.StartInfo.Arguments = "-update-mem " + filename;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.UseShellExecute = false;
+                    if (process.Start())
+                    {
+                        unloadEmbeddedDlls();
+                        Environment.Exit(0);
+                    }
+                    MessageBox.Show("Failed start update MEM insatnce!");
+                }
+
                 loadMD5Tables();
 
                 string iniPath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "installer.ini");
@@ -414,5 +577,6 @@ fail:
             unloadEmbeddedDlls();
             Environment.Exit(0);
         }
+
     }
 }
