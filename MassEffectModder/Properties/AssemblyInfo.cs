e872459a86fb41d3bc71cf36d44b01c4ab883938
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2018-01-01 11:14:07 +0100

    updated mod db

--- MassEffectModder/Properties/AssemblyInfo.cs
+++ MassEffectModder/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a283fe02-fd28-474d-a214-0d8c8c41d062")]
-[assembly: AssemblyVersion("188")]
-[assembly: AssemblyFileVersion("188")]
+[assembly: AssemblyVersion("189")]
+[assembly: AssemblyFileVersion("189")]
