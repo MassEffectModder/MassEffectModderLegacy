1945635c8d9bd6690b3b3e5a8cd26b40d41052f3
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2018-02-24 19:22:05 +0100

    updated permissiongranter

--- MassEffectModder/Properties/AssemblyInfo.cs
+++ MassEffectModder/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a283fe02-fd28-474d-a214-0d8c8c41d062")]
-[assembly: AssemblyVersion("246")]
-[assembly: AssemblyFileVersion("246")]
+[assembly: AssemblyVersion("247")]
+[assembly: AssemblyFileVersion("247")]
