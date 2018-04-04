a5b20b33223ea79d18ceca9742f9b0f457044d06
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2018-04-04 20:30:36 +0200

    bump

--- MassEffectModder/Properties/AssemblyInfo.cs
+++ MassEffectModder/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a283fe02-fd28-474d-a214-0d8c8c41d062")]
-[assembly: AssemblyVersion("265")]
-[assembly: AssemblyFileVersion("265")]
+[assembly: AssemblyVersion("266")]
+[assembly: AssemblyFileVersion("266")]
