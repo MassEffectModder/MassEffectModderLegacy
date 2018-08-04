111093a676b9342c586092c74c2ddbe208f2ab14
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2018-08-04 14:28:26 +0200

    fixed comparer

--- MassEffectModder/TexExplorerReplace.cs
+++ MassEffectModder/TexExplorerReplace.cs
@@ -762,7 +762,7 @@ namespace MassEffectModder
         {
             public int Compare(MapTexturesToMod x, MapTexturesToMod y)
             {
-                return string.CompareOrdinal(x.packagePath, x.packagePath);
+                return string.CompareOrdinal(x.packagePath, y.packagePath);
             }
         }
 
